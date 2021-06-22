7a809d362edeb6ba6671162e97dc7bc5590b1a03
Carlos José jiménez bermúdez <carlos.j@spglobal.com>
2021-06-22 13:16:13 -0600

    Refactor some components for reusability.

---
+++ WindyUI.Blazor/Base/ChildBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindyUI.Base
+{
+    public abstract class ChildBase<T> : ComponentBase
+        where T : class
+    {
+        [CascadingParameter]
+        public T? Parent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (Parent == null)
+                throw new ArgumentNullException(nameof(Parent), $"{this.GetType().Name} must exist within a {typeof(T).Name} component");
+            base.OnInitialized();
+        }
+    }
+}
