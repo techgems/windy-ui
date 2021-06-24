93ae5d08edc77a8fb536f353a82b4fbeb4164e79
Carlos José jiménez bermúdez <carlos.j@spglobal.com>
2021-06-24 17:11:58 -0600

    Start making navigation components more independent.

---
+++ WindyUI.Blazor/Navigation/NavigationColors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindyUI.Navigation
+{
+    public enum NavigationColors
+    {
+        Primary,
+        Secondary
+    }
+
+    public class NavigationColorUtils
+    {
+        public NavigationColors GetSidebarLinkColor(NavigationColors parentColor)
+        {
+            return NavigationColors.Primary;
+        }
+
+        public NavigationColors GetNavbarLinkColor(NavigationColors parentColor)
+        {
+            return NavigationColors.Primary;
+        }
+    }
+}
---
+++ WindyUI.Blazor/Navigation/SidebarColors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindyUI.Navigation
+{
+    public enum SidebarColors
+    {
+        Primary,
+        PrimaryLight,
+        PrimaryDark,
+        Secondary,
+        SecondaryLight,
+        SecondaryDark
+    }
+}
