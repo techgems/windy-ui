e747c0ad51eefa3209c67987ce66bf212039ea79
Carlos José jiménez bermúdez <carlos.j@spglobal.com>
2021-07-02 15:13:18 -0600

    Create base class for all navigation elements.

---
+++ WindyUI.Blazor/Navigation/BaseNavigationLink.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindyUI.Navigation
+{
+    public class BaseNavigationLink : ComponentBase
+    {
+        [Parameter]
+        public NavigationColors Color { get; set; } = NavigationColors.Primary;
+
+        [Parameter]
+        public string CssClass { get; set; } = "";
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public string Route { get; set; } = "";
+
+        protected string InactiveColorClass
+        {
+            get
+            {
+                var colorClass = "";
+
+                switch (Color)
+                {
+                    case NavigationColors.Primary:
+                        colorClass = "bg-primary hover:bg-primary-dark text-primary-text";
+                        break;
+                    case NavigationColors.PrimaryDark:
+                        colorClass = "bg-primary-dark hover:bg-primary text-primary-text";
+                        break;
+                    case NavigationColors.PrimaryLight:
+                        colorClass = "bg-primary-light hover:bg-primary text-primary-text";
+                        break;
+                    case NavigationColors.Secondary:
+                        colorClass = "bg-secondary hover:bg-secondary-dark text-secondary-text";
+                        break;
+                    case NavigationColors.SecondaryDark:
+                        colorClass = "bg-secondary-dark hover:bg-secondary text-secondary-text";
+                        break;
+                    case NavigationColors.SecondaryLight:
+                        colorClass = "bg-secondary-light hover:bg-secondary text-secondary-text";
+                        break;
+                }
+
+                return colorClass;
+            }
+        }
+
+        public string ActiveColorClass
+        {
+            get
+            {
+                var colorClass = "";
+
+                switch (Color)
+                {
+                    case NavigationColors.Primary:
+                        colorClass = "bg-primary-dark";
+                        break;
+                    case NavigationColors.PrimaryDark:
+                    case NavigationColors.PrimaryLight:
+                        colorClass = "bg-primary";
+                        break;
+                    case NavigationColors.Secondary:
+                        colorClass = "bg-secondary-dark";
+                        break;
+                    case NavigationColors.SecondaryDark:
+                    case NavigationColors.SecondaryLight:
+                        colorClass = "bg-secondary";
+                        break;
+                }
+
+                return colorClass;
+            }
+        }
+    }
+}
--- WindyUI.Blazor/Navigation/NavigationColors.cs
+++ WindyUI.Blazor/Navigation/NavigationColors.cs
@@ -9,19 +9,69 @@ namespace WindyUI.Navigation
     public enum NavigationColors
     {
         Primary,
-        Secondary
+        PrimaryLight,
+        PrimaryDark,
+        Secondary,
+        SecondaryLight,
+        SecondaryDark
     }
 
-    public class NavigationColorUtils
+    public static class ColorSelector
     {
-        public NavigationColors GetSidebarLinkColor(NavigationColors parentColor)
+        public static string GetLinkColorFromBackground(NavigationColors backgroundColor)
         {
-            return NavigationColors.Primary;
+            var colorClass = "";
+
+            switch (backgroundColor)
+            {
+                case NavigationColors.Primary:
+                    colorClass = $"text-primary-text hover:bg-primary-dark";
+                    break;
+                case NavigationColors.PrimaryDark:
+                case NavigationColors.PrimaryLight:
+                    colorClass = "text-primary-text hover:bg-primary";
+                    break;
+                case NavigationColors.Secondary:
+                    colorClass = $"text-secondary-text hover:bg-secondary-dark";
+                    break;
+                case NavigationColors.SecondaryDark:
+                case NavigationColors.SecondaryLight:
+                    colorClass = "text-secondary-text hover:bg-secondary";
+                    break;
+            }
+
+            return colorClass;
         }
 
-        public NavigationColors GetNavbarLinkColor(NavigationColors parentColor)
+        public static string GetBackgroundColor(NavigationColors color)
         {
-            return NavigationColors.Primary;
+            var colorClass = "";
+
+            switch (color)
+            {
+                case NavigationColors.Primary:
+                    colorClass = $"bg-primary";
+                    break;
+                case NavigationColors.PrimaryDark:
+                    colorClass = "bg-primary-dark";
+                    break;
+                case NavigationColors.PrimaryLight:
+                    colorClass = "bg-primary-light";
+                    break;
+                case NavigationColors.Secondary:
+                    colorClass = "bg-secondary";
+                    break;
+                case NavigationColors.SecondaryDark:
+                    colorClass = "bg-secondary-dark";
+                    break;
+                case NavigationColors.SecondaryLight:
+                    colorClass = "bg-primary-light";
+                    break;
+            }
+
+            return colorClass;
         }
     }
+
 }
+
