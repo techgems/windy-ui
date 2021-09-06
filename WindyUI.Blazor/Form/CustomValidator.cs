67c3b1c3c390d5820efa0f129aaedfc0c52bed43
Carlos José jiménez bermúdez <carlos.j@spglobal.com>
2021-09-06 10:04:42 -0600

    Fix a few bugs in the library.

--- WindyUI.Blazor/Form/CustomValidator.cs
+++ WindyUI.Blazor/Form/CustomValidator.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindyUI.Form.EditForm
+namespace WindyUI.Form
 {
     public class CustomValidator : ComponentBase
     {
